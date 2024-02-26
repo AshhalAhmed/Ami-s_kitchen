d427260a83b6963cf29f8edd1717654f59ca97a3
ASHHAL AHMED <Ashhalahmed8@gmail.com>
2024-02-26 21:56:39 +0500

    Update models.py

--- final_python_project/recipe_organizer/models.py
+++ final_python_project/recipe_organizer/models.py
@@ -1,8 +1,13 @@
+# Importing necessary modules from Django
 from django.db import models
 
-# Create your models here.
-
+# Define Django model for recipe
 class recipe(models.Model):
-    recipe_name=models.CharField(max_length=100)
-    recipe_text=models.TextField()
-    recipe_picture=models.ImageField(upload_to="recipe_FOLDER")
+    # Define field for storing recipe name as a string with maximum length of 100 characters
+    recipe_name = models.CharField(max_length=100)
+    
+    # Define field for storing recipe text as a large text field
+    recipe_text = models.TextField()
+    
+    # Define field for storing recipe picture as an image file, specifying upload destination
+    recipe_picture = models.ImageField(upload_to="recipe_FOLDER")
